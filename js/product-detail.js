878cf964afae9774b609dbfdee61b83f3e182625
D.Quân <manhquan0105@gmail.com>
2020-07-27 15:00:25 +0700

    up slide vertical

---
+++ js/product-detail.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $(".post-wrapper-4").slick({
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 50000,
+        nextArrow: $(".next-4rd"),
+        prevArrow: $(".prev-4rd"),
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        vertical: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 50000,
+        asNavFor: '.slider-for',
+        focusOnSelect: true,
+        nextArrow: $(".up"),
+        prevArrow: $(".down"),
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
